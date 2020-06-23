8eb21fdcba8620d1bf2fa82507206336b2be2530
h.crane <h.crane.t@gmail.com>
2020-06-23 22:29:13 +0900

    demo

---
+++ Shared/SwiftUI_LazyGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LazyGridApp.swift
+//  Shared
+//
+//  Created by HiromuTsuruta on 2020/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LazyGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
